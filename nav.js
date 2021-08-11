182f3493fa7aef246eed0d6c3686b8f11bff637a
Can202 <mgoopazo@gmail.com>
2021-08-11 09:28:50 -0400

    nav to js

---
+++ nav.js
@@ -0,0 +1,19 @@
+document.write(`
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.html"><img width="60px" src="media/256icon.png"></a>
+                    </li>
+                    <li>
+                        <a href="index.html"><img height="48px" class="navimg" src="media/Home.png"></a>
+                    </li>
+                    <li>
+                        <a href="download.html"><img height="48px" class="navimg" src="media/Download.png"></a>
+                    </li>
+                    <li>
+                        <a href="media.html"><img height="48px" class="navimg" src="media/Media.png"></a>
+                    </li>
+                </ul>
+            </nav>
+
+`)
