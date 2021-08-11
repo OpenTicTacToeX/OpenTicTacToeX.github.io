4b77e07637015b5c8cb8c3f333f10fd21a18f132
Can202 <mgoopazo@gmail.com>
2021-08-10 22:28:32 -0400

    footer in js

---
+++ footer.js
@@ -0,0 +1,28 @@
+document.write(`
+            <footer>
+                <div id="footleft">
+                    <table>
+                        <tr>
+                            <td>People:</td>
+                        </tr>
+                        <tr></tr>
+                        <tr>
+                            <td><a href="https://github.com/Can202/" target="_blank">Can202</a></td>
+                            <td></td>
+                            <td>Hear505</td>
+                        </tr>
+                    </table>
+                </div>
+                <div id="footright">
+                    <a href="https://github.com/Can202/OpenTicTacToeX" target="_blank">
+                        <img height="30px"src="media/link/github.png" class="foottoview">
+                    </a>
+                    <a href="https://sourceforge.net/projects/opentictactoex/" target="_blank">
+                        <img height="30px"src="media/link/sourceforge.png" class="foottoview">
+                    </a>
+                    <a href="https://can202.itch.io/opentictactoex/" target="_blank">
+                        <img height="30px"src="media/link/itch.png" class="foottoview">
+                    </a>
+                </div>
+            </footer>
+`);
